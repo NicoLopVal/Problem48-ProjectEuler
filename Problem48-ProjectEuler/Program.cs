5a7f645920d0a1078502f55d4e8184121f66cc11
nikil <nikil@DESKTOP-73MHGNO>
2022-01-22 20:25:32 -0500

    Add project files.

---
+++ Problem48-ProjectEuler/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+
+BigInteger bigResult = 0;
+int topLimit = 1000;
+int totalLastNumbersToCheck = 10;
+
+for(int i = 1; i <= topLimit; i++)
+{
+    bigResult = bigResult + BigPow(i);
+}
+
+string strResult = bigResult.ToString().Substring(bigResult.ToString().Length - totalLastNumbersToCheck);
+Console.WriteLine("The last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000 is: " + strResult);
+
+BigInteger BigPow(int value)
+{
+    BigInteger result = 1;
+    for(int i = 0; i < value; i++)
+    {
+        result = result * value;
+    }
+    return result;
+}
